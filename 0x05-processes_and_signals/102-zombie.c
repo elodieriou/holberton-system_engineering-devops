cdbd6f5a55828881f6eec438d88db2fbeb6bbd0a
Elodie Riou <elodie.riou@hotmail.com>
2022-02-18 22:18:03 +0100

    refactoring code

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -6,7 +6,7 @@
 
 /**
  * infinite_while - Function that create an infinite loop
- * Return: Nothing
+ * Return: Always success
  */
 int infinite_while(void)
 {
@@ -18,23 +18,30 @@ int infinite_while(void)
 }
 
 /**
+ * zombie_process - Function that create a zombie process
+ * Return: Nothing
+ */
+void zombie_process(void)
+{
+	pid_t child_pid = fork();
+
+	if (child_pid == 0)
+		exit(0);
+	else
+		printf("Zombie process created, PID: %d\n", child_pid);
+}
+
+/**
  * main - Program that create 5 zombie process
  * Return: Always success
  */
 int main(void)
 {
-	pid_t child_pid;
-	int i = 0;
+	int i;
 
-	while (i < 5)
-	{
-		child_pid = fork();
-		if (child_pid == 0)
-			exit(0);
-		else
-			printf("Zombie process created, PID: %d\n", child_pid);
-		i++;
-	}
+	for (i = 0; i < 5; i++)
+		zombie_process();
+
 	infinite_while();
 	return (0);
 }
