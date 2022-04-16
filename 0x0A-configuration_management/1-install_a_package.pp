b4240ad77791eb167c70fc215ef70c382fae506d
Elodie Riou <elodie.riou@hotmail.com>
2022-04-16 10:34:14 +0200

    fix code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  name     => 'puppet-lint',
-  provider => 'gem',
+exec { 'puppet-lint':
+  command => 'sudo gem install puppet-lint -v 2.5.0',
+  path    => '/usr/bin',
   }
