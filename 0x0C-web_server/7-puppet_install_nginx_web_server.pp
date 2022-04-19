b5da5076dca9dcd41c30af52e5bf9584fa6d69ee
Elodie Riou <elodie.riou@hotmail.com>
2022-04-19 21:14:57 +0200

    add file for task 6

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# Install Nginx web server (w/ Puppet)
+include stdlib
+
+exec { 'install nginx':
+  command => 'sudo apt-get -y install nginx',
+  path => '/usr/bin',
+  }
+
+file { 'hello world':
+  content => 'Hello World',
+  }
+
+file_line { 'redirection 301 moved permanently':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default'
+  after  => 'server_name _;'
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+  }
+
+service { 'restart nginx':
+  ensure  => running,
+  restart => 'usr/sbin/nginx restart',
+  }
