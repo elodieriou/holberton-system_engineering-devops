efa01a169ecdec91a0621b646bd34debd94ac4fc
Elodie Riou <elodie.riou@hotmail.com>
2022-04-19 21:39:24 +0200

    fix code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,15 +4,15 @@ package { 'nginx':
   ensure => installed,
 }
 
-file { 'var/www/html/index.nginx-debian.html':
-  content => 'Hello World',
-}
-
 file_line { 'redirection 301 moved permanently':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => '/server_name _;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { 'var/www/html/index.nginx-debian.html':
+  content => 'Hello World',
 }
 
 service { 'nginx':
