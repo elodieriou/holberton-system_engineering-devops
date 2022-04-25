1f288bc8c96843bc75dda19b25a8ea10c5476ac8
Elodie Riou <elodie.riou@hotmail.com>
2022-04-25 17:11:32 +0200

    add file for task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,33 @@
+# Automate the task of creating a custom HTTP header response
+#+ The name of the custom HTTP header must be X-Served-By
+#+ The value of the custom HTTP header must be the hostname of the server Nginx is running on
+#+ It configures a brand new Ubuntu machine to the requirements asked in this task
+
+exec { 'update':
+  command => 'apt-get update',
+  path    => '/usr/bin',
+  }
+
+exec { 'upgrade':
+  command => 'apt-get -y upgrade',
+  path    => '/usr/bin',
+  }
+
+package { 'nginx':
+  ensure  => present,
+  name    => 'nginx',
+  require => Exec['update'],
+  }
+
+file_line { 'Add header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => "add_header X-Served-By ${HOSTNAME};",
+  require => Package['nginx'],
+  }
+
+service { 'nginx':
+ensure  => running,
+require => Package['nginx'],
+  }
