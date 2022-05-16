a2006ffaf7d3e59d9341ae48eeaf6a727374dfae
Elodie Riou <elodie.riou@hotmail.com>
2022-05-16 16:44:59 +0200

    fix code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a script that using an REST API, for a given employee ID
-and returns information about his/her TODO list progress"""
+and returns information about his/her"""
 
 if __name__ == "__main__":
     import csv
@@ -17,10 +17,11 @@ if __name__ == "__main__":
 
     user_name = req_user.get('username')
 
-    filename = "{}.csv".format(user_id)
+    filename = "{}.csv".format(argv[1])
     with open(filename, 'w', encoding='UTF8') as f:
         csv_writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
 
         for value in req_todos:
-            data = [user_id, user_name, value.get('completed'), value.get('title')]
+            data = [user_id, user_name, value.get('completed'),
+                    value.get('title')]
             csv_writer.writerow(data)
