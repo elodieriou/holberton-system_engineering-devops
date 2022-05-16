c51839a48c47d00994b82512e49ec7c2f7d0ff84
Elodie Riou <elodie.riou@hotmail.com>
2022-05-16 16:51:24 +0200

    executable file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""
-Python script that exports data using the CSV format.
-"""
-
-import csv
-import requests
-from sys import argv
+"""This module defines a script that using an REST API, for a given employee ID
+and returns information about his/her"""
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    api_url = requests.get(url)
-    username = api_url.json().get("username")
-    uid = api_url.json().get("id")
+    import csv
+    import requests
+    from sys import argv
 
-    api_url_todo = requests.get("{}/todos".format(url))
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = argv[1]
+    user_url = "{}/users/{}".format(url, user_id)
+    todos_url = "{}/todos".format(user_url)
 
-    filename = "{}.csv".format(argv[1])
+    req_user = requests.get(user_url).json()
+    req_todos = requests.get(todos_url).json()
 
-    with open(filename, "w", encoding='UTF8') as f:
-        writer = csv.writer(f, quotechar="'")
+    user_name = req_user.get('username')
 
-        for value in api_url_todo.json():
-            row = ['\"{}\"'.format(uid), '\"{}\"'.format(username)]
-            row.append('\"{}\"'.format(value.get("completed")))
-            row.append('\"{}\"'.format(value.get("title")))
-            writer.writerow(row)
+    filename = "{}.csv".format(user_id)
+    with open(filename, 'w', encoding='UTF8') as f:
+        csv_writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+
+        for value in req_todos:
+            data = [user_id, user_name, value.get('completed'),
+                    value.get('title')]
+            csv_writer.writerow(data)
