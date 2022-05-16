99ce94733dc847152085c2418766d415f8c2a66c
Elodie Riou <elodie.riou@hotmail.com>
2022-05-16 16:38:05 +0200

    fix code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,13 +3,13 @@
 and returns information about his/her TODO list progress"""
 
 if __name__ == "__main__":
-    from sys import argv
     import csv
     import requests
+    from sys import argv
 
     # formatting url
     url = "https://jsonplaceholder.typicode.com"
-    user_id = int(argv[1])
+    user_id = argv[1]
     user_url = "{url}/users/{id}".format(url=url, id=user_id)
     todos_url = "{user_url}/todos".format(user_url=user_url)
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     # export to csv format
     filename = "{}.csv".format(user_id)
-    with open(filename, 'w', encoding='utf-8', newline='') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         csv_writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
 
         for value in req_todos:
