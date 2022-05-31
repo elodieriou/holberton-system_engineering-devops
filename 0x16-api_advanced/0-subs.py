483a8a4a3a1cbd4809975307ee616d6bfa8ef269
Elodie Riou <elodie.riou@hotmail.com>
2022-05-31 11:10:19 +0200

    add file for task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines the list of all subscribers on reddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """This method requests the api reddit to return the number
+    of all subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'elodieriou3685'}
+    req_reddit = requests.get(url, headers=headers, allow_redirects=False)
+    if req_reddit.status_code == 404:
+        return 0
+    subscribers = req_reddit.json().get('data').get('subscribers')
+    return subscribers
