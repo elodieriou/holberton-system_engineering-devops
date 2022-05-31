f49dafa228f92bf6819fe8907e8300ca9a202901
Elodie Riou <elodie.riou@hotmail.com>
2022-05-31 11:44:47 +0200

    fix code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,10 @@ def number_of_subscribers(subreddit):
     of all subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'elodieriou3685'}
+
     req_reddit = requests.get(url, headers=headers, allow_redirects=False)
-    if req_reddit.status_code == 404:
+    if req_reddit.status_code > 300:
         return 0
+
     subscribers = req_reddit.json().get('data').get('subscribers')
     return subscribers
