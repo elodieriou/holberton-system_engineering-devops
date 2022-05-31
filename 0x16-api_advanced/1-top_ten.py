b1ab15393483ff917405c3e63f39bdad71a57584
Elodie Riou <elodie.riou@hotmail.com>
2022-05-31 11:45:00 +0200

    add file for task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines the list of all subscribers on reddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """This method requests the api reddit to return the top ten
+    hot post for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'elodieriou3685'}
+    params = {'limit': 10}
+
+    req_reddit = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if req_reddit.status_code > 300:
+        print("None")
+        return
+
+    hot = req_reddit.json().get('data').get('children')
+    for top in hot:
+        print(top.get('data').get('title'))
